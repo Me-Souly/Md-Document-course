1e99089afb67d79d0bfb5ff3c40cc7fb7b5f89d1
Souly Mironov <dan1la.mironov@yandex.by>
2025-10-05 15:18:54 +0300

    add error handler, upd services and user-controller

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -3,12 +3,30 @@ const tokenModel = require('../models/token-model');
 
 class TokenService {
     generateTokens(payload) {
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '45m'});
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '30m'});
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '45d'});
         
         return {
             accessToken,
             refreshToken
+        }
+    }
+
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
         }
     }
 
@@ -21,7 +39,17 @@ class TokenService {
         }
         const token = await tokenModel.create({user:userId, refreshToken});
         return token;
-    } 
+    }
+
+    async removeToken(refreshToken) {
+        const tokenData = await tokenModel.deleteOne({refreshToken})
+        return tokenData;
+    }
+
+    async findToken(refreshToken) {
+        const tokenData = await tokenModel.findOne({refreshToken})
+        return tokenData;
+    }
 }
 
 module.exports = new TokenService();
