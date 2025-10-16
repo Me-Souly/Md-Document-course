e600ff23b6f913e8d87571375b6b62fa6a4642fa
Souly Mironov <dan1la.mironov@yandex.by>
2025-10-16 15:44:41 +0300

    upd use separated controllers in router

--- server/router/index.js
+++ server/router/index.js
@@ -1,20 +1,21 @@
 const Router = require('express').Router;
-const userController = require('../controllers/user-controller');
 const router = new Router();
 const {body} = require('express-validator');
 const authMiddleware = require('../middlewares/auth-middleware');
 const moderatorMiddleware = require('../middlewares/moderator-middleware');
 const activatedMiddleware = require('../middlewares/activated-middleware');
+const { userController, authController, activationController } = require('../controllers');
 
 router.post('/registration', 
     body('email', 'Email is incorrect').isEmail(),
     body('username', 'Minimal username length is 2').isLength({min: 2}),
     body('password', 'Password length should be between 3 and 32').isLength({min: 3, max: 32}),
     userController.registration);
-router.post('/login', userController.login);
-router.post('/logout', userController.logout);
-router.get('/activate/:link', userController.activate);
-router.get('/refresh', userController.refresh);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.get('/activate/:link', activationController.activate);
+router.get('/refresh', authController.refresh);
+router.get('/reset/:link', userController.reset);
 router.get('/users', authMiddleware, activatedMiddleware, userController.getUsers);
 
 module.exports = router;
